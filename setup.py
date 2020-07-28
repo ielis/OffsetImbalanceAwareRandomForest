29d1a98d772fbb99294c5605f77c435491f7ebea
Daniel Danis <daniel.danis@jax.org>
2020-07-28 13:05:55 +0200

    Initial setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# read requirements
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# read description
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setup(name='oias',
+      version='0.0.1.dev0',
+      packages=find_packages(),
+      setup_requires=['wheel>=0.34.0'],
+      install_requires=requirements,
+
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+
+      author='Daniel Danis',
+      author_email='daniel.gordon.danis@protonmail.com',
+      url='https://github.com/ielis/OffsetImbalanceAwareRandomForest',
+      description='Offset and imbalance-aware random forest classifier',
+      license='GPLv3',
+      keywords='bioinformatics genomics algorithm',
+
+      package_data={
+          '': ['test_data/*']},
+      data_files=[('', ['requirements.txt'])]
+      )
